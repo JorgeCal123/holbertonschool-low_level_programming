55534b18d7aa3aa5f78ce41ef3010648b03658ea
JorgeCal123/boom.git <git@github.com>
2021-09-30 18:03:22 -0700

    punto 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: "+ fizeof(char) +" byte(s)\n")
+printf("Size of a char: "+ fizeof(int) +" byte(s)\n")
+printf("Size of a char: "+ fizeof(long int) +" byte(s)\n")
+printf("Size of a char: "+ fizeof(long long int) +" byte(s)\n")
+printf("Size of a char: "+ fizeof(float) +" byte(s)\n")
+    return (0);
+}
