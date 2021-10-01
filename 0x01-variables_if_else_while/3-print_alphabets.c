761bcaa151af895d9e160acd9d18a20f3ce2e5b6
JorgeCal123/boom.git <git@github.com>
2021-10-01 15:36:24 -0700

    punto 3 corr 1

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,21 +5,20 @@
  */
 int main(void)
 {
-	char letter='a';
-	char letter1='A';
-        while (letter <= 'z')
-        {
-                putchar(letter);
-                letter++;
-        }
+	char letter = 'a';
+	char letter1 = 'A';
+
+	while (letter <= 'z')
+	{
+	putchar(letter);
+	letter++;
+	}
 	while (letter1 <= 'Z')
-        {
-                putchar(letter1);
-                letter1++;
-        }
-
-
-        putchar('\n');
-        return (0);
+	{
+	putchar(letter1);
+	letter1++;
+	}
+	putchar('\n');
+	return (0);
 }
 
