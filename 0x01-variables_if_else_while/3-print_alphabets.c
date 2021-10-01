a69af1b9a5a3f049d2b906733fbdfda084128092
JorgeCal123/boom.git <git@github.com>
2021-10-01 15:30:10 -0700

    punto 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - print
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter='a';
+	char letter1='A';
+        while (letter <= 'z')
+        {
+                putchar(letter);
+                letter++;
+        }
+	while (letter1 <= 'Z')
+        {
+                putchar(letter1);
+                letter1++;
+        }
+
+
+        putchar('\n');
+        return (0);
+}
+
