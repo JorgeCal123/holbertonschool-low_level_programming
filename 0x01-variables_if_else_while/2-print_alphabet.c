074e3b4c9a7620cfccfe4f78206819f2b0aefb41
JorgeCal123/boom.git <git@github.com>
2021-10-01 13:42:52 -0700

    punto 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter1=’a’;
+	char letter2=’Z’;
+	while (letter1 < letter2)
+		putchar(letter1);
+		letter1++;
+	}
+	putchar('\n');
+	return (0);
+} 
