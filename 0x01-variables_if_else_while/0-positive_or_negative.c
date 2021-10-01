b5b88ec229411e3f24d09709c724a19f4b43c91e
JorgeCal123/boom.git <git@github.com>
2021-10-01 10:11:10 -0700

    punto 0 corr 2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,13 +10,13 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if(n<0){
-printf("%d%s", n, " is negative\n");
+printf("%d is negative\n", n);
 }
 else if(n==0){
-printf("%d%s", n, " is zero\n");
+printf("%d is zero\n", n);
 }
 else{
-printf("%d%s",n, " is positive\n");
+printf("%d is positive\n", n);
 }
 return (0);
 }
