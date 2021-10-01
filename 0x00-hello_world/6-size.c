2daeca1916db12944b22922b1b28d8655bea0bd0
JorgeCal123/boom.git <git@github.com>
2021-09-30 18:16:53 -0700

    punto 6 corr 4

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,9 +8,9 @@
 int main(void)
 {
 printf("Size of a char: ", sizeof(char), " byte(s)\n");
-printf("Size of a char: ", sizeof(int), " byte(s)\n");
-printf("Size of a char: ", sizeof(long int), " byte(s)\n");
-printf("Size of a char: ", sizeof(long long int), " byte(s)\n");
-printf("Size of a char: ", sizeof(float), " byte(s)\n");
+printf("Size of an int: ", sizeof(int), " byte(s)\n");
+printf("Size of a long int: ", sizeof(long int), " byte(s)\n");
+printf("Size of a long long int ", sizeof(long long int), " byte(s)\n");
+printf("Size of a float: ", sizeof(float), " byte(s)\n");
     return (0);
 }
