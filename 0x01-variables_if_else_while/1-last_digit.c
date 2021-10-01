5ebf549d56825bad28d9f209f730fafcd4037e0f
JorgeCal123/boom.git <git@github.com>
2021-10-01 13:04:08 -0700

    punto 1 corr 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,26 +1,33 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - print
+ * 
+ * Return: Always 0 (Success)
+ */
+
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	char n1[] = "Last digit of %d", n ;
 	int num = n%10;
 
 	if (num > 5)
-		printf(n1," is ",num, " and is greater than 5" );
+		printf("Last digit of %d is %d and is greater than 5\n", n, num);
 	else if (num < 6)
-	}
-		printf(n1," is ",num, " and is less than 6" );
+	{
+		printf("Last digit of %d is %d and is less than 6\n",n, num);
 	}
 	else
 	{
-		printf(n1," is ",num, " and is 0" );
+		printf("Last digit of %d is %d and is 0\n",n, num);
 	}
 
 	return (0);
