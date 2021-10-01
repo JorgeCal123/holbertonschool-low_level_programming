054a3c5197c7ed457beee6dc60768b5b2503fdcb
JorgeCal123/boom.git <git@github.com>
2021-10-01 13:09:20 -0700

    punto 1 corr 5

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,8 +17,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int num = n % 10;
-
 	if (num > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	else if (num < 6)
