5bdd5fab04d3629c248c63b374921e4aa780dcf7
JorgeCal123/boom.git <git@github.com>
2021-10-01 13:21:08 -0700

    punto 1 corr 7

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,6 @@
 /* betty style doc for function main goes there */
 /**
  * main - print
- * 
  * Return: Always 0 (Success)
  */
 
@@ -14,7 +13,6 @@
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n % 10 > 5)
