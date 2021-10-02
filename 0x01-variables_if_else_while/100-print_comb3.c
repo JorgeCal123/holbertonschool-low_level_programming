eb242c901499f4a8b5092ea974aa19810a4b24fe
JorgeCal123/boom.git <git@github.com>
2021-10-01 21:02:23 -0700

    punto 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - print
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int cont1;
+	int cont2;
+	int tmp = 1;
+	for (cont1 = 0 ; cont1 < 9 ; cont1 ++)
+	{
+		for (cont2 = tmp ; cont2 <= 9 ; cont2 ++)
+		{
+			
+			putchar(cont1 + '0');
+			putchar(cont2 + '0');
+			if (cont1 != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		tmp++;
+	
+	}
+        putchar('\n');
+        return (0);
+}
