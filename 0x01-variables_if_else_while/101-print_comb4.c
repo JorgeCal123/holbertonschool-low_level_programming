d179e23f7ab1416ee7951a78f6952c2102028213
JorgeCal123/boom.git <git@github.com>
2021-10-02 07:46:34 -0700

    punto 11 corr 5

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,31 +3,30 @@
  * main - print
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-        int cont1;
-        int cont2;
-        int cont3;
+	int cont1;
+	int cont2;
+	int cont3;
 
-
-        for (cont1 = 0 ; cont1 < 9 ; cont1++)
-        {
-                for (cont2 = cont1 + 1 ; cont2 <= 8; cont2++)
-                {
+	for (cont1 = 0 ; cont1 < 9 ; cont1++)
+	{
+		for (cont2 = cont1 + 1 ; cont2 <= 8; cont2++)
+		{
 			for(cont3 = cont2 + 1 ; cont3 <= 9 ; cont3++)
-                        {
-                        putchar(cont1 + '0');
-                        putchar(cont2 + '0');
-                        putchar(cont3 + '0');
-			if (cont1 != 7)
-                        {
-                                putchar(',');
-                                putchar(' ');
-                        }
-
+			{
+				putchar(cont1 + '0');
+				putchar(cont2 + '0');
+				putchar(cont3 + '0');
+				if (cont1 != 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
-        }
-        putchar('\n');
-        return (0);
+	}
+	putchar('\n');
+	return (0);
 }
