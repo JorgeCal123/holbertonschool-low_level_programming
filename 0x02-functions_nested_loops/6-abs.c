cb0b06aad06e3cdaa5562c89b4c7dec4185fbd9e
JorgeCal123/boom.git <git@github.com>
2021-10-05 10:09:15 -0700

    punto 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,13 @@
-#include <_putchar>
+#include "main.h"
 /**
- *
- *
- *
+ * _abs -  computes the absolute value of an integer
+ * @a: voalu or integer
+ * Return: absolute value
  */
 int _abs(int a)
 {
 	int answer = a;
+
 	if (a < 0)
 	{
 		answer = a * (-1);
