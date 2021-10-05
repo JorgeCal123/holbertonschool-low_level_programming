d88d782a2e5919b817d9fb292c18378edd65d375
JorgeCal123/boom.git <git@github.com>
2021-10-05 09:49:59 -0700

    punto 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,23 +1,20 @@
-#include <_putchar>
+#include <main.h>
 /**
- *
- *
- *
+ * print_sign -  prints the sign of a number.
+ * @n: sign of a number
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
 	int answer;
-	
-	if(n == 0)
-	{
-		answer=('0');
-	}
-	else if(n < 0 )
-	{
-		answer=('-');
-	}
-	else{
-		answer=('+');
-	}
-	return(answer);
+
+	if (n == 0)
+		answer = ('0');
+	else if (n < 0)
+		answer = ('-');
+	else
+		answer = ('+');
+	return (answer);
 }
