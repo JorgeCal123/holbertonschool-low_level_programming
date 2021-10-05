e0c2d6bf38a8012a13ce7d7e7bd3e7e9603327bd
JorgeCal123/boom.git <git@github.com>
2021-10-05 10:18:08 -0700

    punto 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number.
+ * @a: input value
+ *
+ * Return: the value of the last digit
+ */
+int print_last_digit(int a)
+{
+	return (a % 10);
+}
