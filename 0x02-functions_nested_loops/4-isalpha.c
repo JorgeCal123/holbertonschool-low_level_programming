ecfb5b16ebd65d36d0109d504d1d05cc209e0504
JorgeCal123/boom.git <git@github.com>
2021-10-05 09:39:08 -0700

    punto 4 corr 3

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,12 +8,12 @@ int _isalpha (int c)
 {
 	int answer;
 
-	if((c >= 'a' && c<= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c<= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		answer = 1;
 	}
 	else{
 		answer = 0;
 	}
-	retunr(answer);
+	return(answer);
 }
