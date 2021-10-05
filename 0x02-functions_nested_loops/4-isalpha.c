fb55b49f35b97ecdc03d22b8269bc6541be17d55
JorgeCal123/boom.git <git@github.com>
2021-10-05 09:41:56 -0700

    punto 4 corr 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,19 +1,16 @@
 #include "main.h"
 /**
  * _isalpha - Write a function that checks for alphabetic character.
- * @c: character 
+ * @c: character
  * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
  */
-int _isalpha (int c)
+int _isalpha(int c)
 {
 	int answer;
 
-	if ((c >= 'a' && c<= 'z') || (c >= 'A' && c <= 'Z'))
-	{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		answer = 1;
-	}
-	else{
+	else
 		answer = 0;
-	}
-	return(answer);
+	return (answer);
 }
