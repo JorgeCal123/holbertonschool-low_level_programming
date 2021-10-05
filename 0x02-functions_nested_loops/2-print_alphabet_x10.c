f39320a97bfb64024849a611e50756bd5be96525
JorgeCal123/boom.git <git@github.com>
2021-10-05 09:05:39 -0700

    punto 2 corr 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * 10 x alphabet - prints 10 times the alphabet, in lowercase, followed by a new line.
+ * 10_x_alphabet - prints ten times the alphabet, in lowercase, followed by a new line.
  */
 void print_alphabet_x10()
 {
 	int i;
 	char j;
 
-	for (i=0 ; i < 10 ; i++)
+	for (i = 0 ; i < 10 ; i++)
 	{
-		for (j='a' ; j <= 'z' ; j++)
+		for (j = 'a' ; j <= 'z' ; j++)
 		{
 			_putchar(j);
 		}
