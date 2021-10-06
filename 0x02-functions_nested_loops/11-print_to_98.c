c781e502fe36dd12d8172a0c078ddfc7b8ba6457
JorgeCal123/boom.git <git@github.com>
2021-10-05 19:01:33 -0700

    punto 11 corr 6

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,16 +10,25 @@ void print_to_98(int n)
 
 	if (n == 98)
 	{
-		printf("%d\n", n);
+		printf(n);
 	}
 	else if (n < 98)
 	{
 		for (i = n; i <= 98; i++)
-			printf("%d, ", i);
+			printf("%d", i);
+		if (n != 98)
+			{
+				printf(", ");
+			}
 	}
 	else
 	{
 		for (j = n; j >= 98 ; j--)
-			printf("%d, ", j);
+			printf("%d", j);
+		if (n != 98)
+			{
+				printf(", ");
+			}
+
 	}
 }
