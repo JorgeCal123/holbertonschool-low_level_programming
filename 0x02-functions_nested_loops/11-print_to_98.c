ba853d8c9589f3314b5f8cad3765846e7eedfba2
JorgeCal123/boom.git <git@github.com>
2021-10-05 18:38:03 -0700

    punto 11 corr 1

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,10 +1,13 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 - prints all natural numbers from n to 98
  * @n: limit number that prints
  */
 void print_to_98(int n)
 {
+	int i, j;
+
 	if (n == 98)
 	{
 		printf(n);
