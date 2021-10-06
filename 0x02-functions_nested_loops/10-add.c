348a8e41c091e23735091ed481ff9fbc946b45da
JorgeCal123/boom.git <git@github.com>
2021-10-05 18:22:15 -0700

    punto 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - two integers and returns the result.
+ * @a: first value
+ * @b: second value
+ * Return: adds two integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
