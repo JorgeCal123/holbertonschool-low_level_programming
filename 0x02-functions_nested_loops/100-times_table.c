c946b235cb419fbc0e350a0456e3530a29725c29
JorgeCal123/boom.git <git@github.com>
2021-10-06 10:15:38 -0700

    punto 100 corr 3

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,12 +10,12 @@ void print_times_table(int n)
 
 	if (n < 15 || n > 0)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i <= n; i++)
 		{
 			_putchar('0');
 			_putchar(',');
 			_putchar(' ');
-			for (j = 1; j < n; j++)
+			for (j = 1; j <= n; j++)
 			{
 				k = i * j;
 				if (k < 10)
