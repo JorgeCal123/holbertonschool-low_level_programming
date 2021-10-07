4eedfd902979a9abfa48bc815da1c101f86b1f6a
JorgeCal123/boom.git <git@github.com>
2021-10-06 21:29:50 -0700

    punto 104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -  finds and prints the first 98 Fibonacci numbers
+ * Return: value 0
+ */
+int main(void)
+{
+	int i;
+	long a = 1;
+	long s = 2;
+	long temp;
+
+	for (i = 0; i <98; i++)
+	{
+		if (i < 97)
+			printf("%ld, ", a);
+		else
+			printf("%ld\n", a);
+	temp = a;
+	a = s;
+	s = temp + s;
+	}
+	return (0);
+}
