66d52824ffe4279212e622c55c800fcf1114de5e
JorgeCal123/boom.git <git@github.com>
2021-10-07 07:16:24 -0700

    punto 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - tests function that prints is is positive or negative
+* when the input is zero
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
+}
+
