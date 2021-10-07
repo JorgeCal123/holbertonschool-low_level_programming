bdcfdc9880c7c720e7728ec45c9dcde637141172
JorgeCal123/boom.git <git@github.com>
2021-10-06 18:01:01 -0700

    punto 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * multiples_of_3_5 - prints the sum of all the multiples of 3 or 5 below 1024
+ */
+void multiples_of_3_5(void)
+{
+	int i = 0;
+	while(i < 1024)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			printf("%d ", i);
+		}
+	i++;
+	}
+}
