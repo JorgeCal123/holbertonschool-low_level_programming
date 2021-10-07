0325aae81a50e8fbf93833b4aa6cea04fe8eba8b
JorgeCal123/boom.git <git@github.com>
2021-10-06 18:03:46 -0700

    punto 101 corr 2

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,7 +2,7 @@
 /**
  * main - prints the sum of all the multiples of 3 or 5 below 1024
  */
-void main(void)
+int main(void)
 {
 	int i = 0;
 
@@ -14,4 +14,5 @@ void main(void)
 		}
 	i++;
 	}
+	return (0);
 }
