16ea4f91851d98e1b7a01e51dfccd0791052c2ad
JorgeCal123/boom.git <git@github.com>
2021-10-06 18:12:41 -0700

    punto 101 corr 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
  * main - prints the sum of all the multiples of 3 or 5 below 1024
+ * Return: value 0
  */
 int main(void)
 {
 	int i = 0;
 	int suma;
 
-	while(i < 1024)
+	while (i < 1024)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 		{
