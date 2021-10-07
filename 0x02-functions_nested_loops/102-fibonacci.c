d86764d7fc749a7b282f21438ed91230a413a700
JorgeCal123/boom.git <git@github.com>
2021-10-06 19:42:42 -0700

    punto 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints the first 50 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
+ * Return: value 0
+ */
+int main(void)
+{
+	int i;
+	int a = 1;
+	int s = 1;
+	int temp;
+
+	for (i = 0; i <= 50; i++)
+	{
+		printf(a);
+		temp = a;
+		a = s;
+		s = temp + s;
+	}
+	return (0);
+}
