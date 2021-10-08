fe76235ab8447a77d06e20a4c879651cc3df98a1
JorgeCal123/boom.git <git@github.com>
2021-10-08 14:53:26 -0700

    punto 2

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isdigit -  checks for a digit.
+ * @c: int for check
+ * Return: 1 if c is a digit or 0 otherwise
+ */
+int _isdigit(int c)
+{
+	int answer;
+
+	if(c < 10)
+		answer = 1;
+	else
+		answer = 0;
+	return (answer);
+}
