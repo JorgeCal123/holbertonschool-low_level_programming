a5bc485a5f8c874ccab88793997a4a8e28ba306b
JorgeCal123/boom.git <git@github.com>
2021-10-08 14:30:22 -0700

    punto 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * Write a function that checks for uppercase character.
+ * @c: character 
+ * Return: 1 if c is uppercase or 0 otherwise
+ */
+int _isupper(char c)
+{
+	char inicio;
+	int resp;
+	int fin = 0;
+
+	for (inicio = 'A'; inicio 'Z' && fin == 0; inicio++)
+	{
+		if (c == inicio){
+			resp = 1;
+			fin = 1;
+		}
+		else 
+		{
+			resp = 0;
+		}
+	}
+	return resp;
+}
