82fef8cf04bd29a2c39bdf77dc582c73142d2e28
JorgeCal123/boom.git <git@github.com>
2021-10-08 15:04:09 -0700

    punto 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - function that multiplies two integers.
+ * @a: first value int
+ * @b: second value int
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
