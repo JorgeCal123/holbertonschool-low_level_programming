9047db36c95b1dcf5af8d6990876daccc41f8812
JorgeCal123/boom.git <git@github.com>
2021-10-08 21:23:30 -0700

    punto 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,8 +12,8 @@ void print_number(int n)
 	}
 	if (n > 10 && n < 100)
 	{
-		_putchar((n / 10)+ '0');
-		_putchar((n % 10)+ '0');
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
 	}
 	else if (n > 100 && n < 1000)
 	{
