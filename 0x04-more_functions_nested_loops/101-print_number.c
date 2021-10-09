76134101531ee2bebd17016486eb7a45809c38e2
JorgeCal123/boom.git <git@github.com>
2021-10-08 21:22:30 -0700

    punto 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_number - Write a function that prints an integer witch a _putchar.
+ * @n: input integer
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	if (n > 10 && n < 100)
+	{
+		_putchar((n / 10)+ '0');
+		_putchar((n % 10)+ '0');
+	}
+	else if (n > 100 && n < 1000)
+	{
+		_putchar((n / 100) + '0');
+		_putchar((n / 10) % 10 + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if (n > 1000 && n < 10000)
+	{
+		_putchar((n / 1000) + '0');
+		_putchar((n / 100) % 10 + '0');
+		_putchar((n / 10) % 10 + '0');
+		_putchar((n % 10) + '0');
+	}
+	else
+	{
+		_putchar(n + '0');
+	}
+}
