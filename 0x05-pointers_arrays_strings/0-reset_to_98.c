e90750fcbf1c7993436387bccfec11b92d5c61e6
JorgeCal123/boom.git <git@github.com>
2021-10-11 12:48:11 -0700

    punto 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * function that takes a pointer to an int as parameter and
+ * reset_to_98 - function that takes a pointer to an int as parameter and
  * updates the value it points to to 98.
  * @n: value it points to to 98.
  */
