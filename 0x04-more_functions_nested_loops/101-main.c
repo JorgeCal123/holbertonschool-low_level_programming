8d4b844e086695703cbe26fccc4ff0ce7c5469a5
JorgeCal123/boom.git <git@github.com>
2021-10-11 12:47:03 -0700

    punto 0

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -7,15 +7,21 @@
  */
 int main(void)
 {
-    print_number(98);
-    _putchar('\n');
-    print_number(402);
-    _putchar('\n');
-    print_number(1024);
-    _putchar('\n');
     print_number(0);
     _putchar('\n');
-    print_number(-98);
+    print_number(4);
     _putchar('\n');
+    print_number(14);
+    _putchar('\n');
+    print_number(123);
+    _putchar('\n');
+    print_number(452);
+    _putchar('\n');
+     print_number(4542);
+    _putchar('\n');
+     print_number(-452);
+    _putchar('\n');
+
+
     return (0);
 }
