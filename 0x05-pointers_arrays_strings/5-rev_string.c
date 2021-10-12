c15ca9cf51325e1845a8a968e68b1d4626f435d0
JorgeCal123/boom.git <git@github.com>
2021-10-11 19:58:16 -0700

    corr 4 punto 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,15 +7,15 @@
 void rev_string(char *s)
 {
 	int i = 0;
-        int k = 0;
+	int k = 0;
 	char tmp;
 
-        while (s[i] != '\0')
-        {
-                i++;
-        }
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 
-	for (k = i - 1; k >= i/2; k-- )
+	for (k = i - 1; k >= i / 2; k--)
 	{
 		tmp = s[k];
 		s[k] = s[i - k - 1];
