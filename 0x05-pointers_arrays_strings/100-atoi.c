2e19aea8c2d5644d996cb1d1112819c4ce285f33
JorgeCal123/boom.git <git@github.com>
2021-10-12 19:18:08 -0700

    punto 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  * _atoi - Write a function that convert a string to an integer.
  * @s: into value String
@@ -11,20 +11,24 @@ int _atoi(char *s)
 	int parte1 = 0;
 	int parte2 = 0;
 	int i = 0;
+	int terminar = 1;
 
-	while (s[i] != '\0')
+	while (s[i] != '\0' && terminar)
 	{
-
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			if (s[i - 1] != '\0' && s[i - 1] == '-')
-				signo *= - 1 ;
-
+			if (i != 0)
+			{
+				if (s[i - 1] == '-')
+				signo *= -1;
+			}
 				parte1 = resultado * 10;
 				parte2 = (s[i] - '0');
 				resultado = (parte1 + parte2);
-        	}
+		}
+		else if (resultado > 0)
+			terminar = 0;
 	i++;
 	}
-	return (signo*resultado);
+	return (signo * resultado);
 }
