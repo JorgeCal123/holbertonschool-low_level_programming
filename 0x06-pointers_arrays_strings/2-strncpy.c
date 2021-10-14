807a56641f21c44db15f381deedb0f549058d3f9
JorgeCal123/boom.git <git@github.com>
2021-10-14 16:32:05 -0700

    punto 2 corr 1

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,27 +9,20 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-	int j = 0;
 	int k = 0;
 
 	while (dest[i] != '\0')
 		i++;
 
-	if (i < n)
+	while (k < n)
 	{
-		while (src[j] != '\0')
+		dest[k] = src[k];
+
+		if (k > i)
 		{
-		dest[j] = src[j];
-		j++;
+			dest[k] = '\0';
 		}
-	}
-	else
-	{
-		while (k < n)
-		{
-			dest[k] = src[k];
-			k++;
-		}
+	k++;
 	}
 	return (dest);
 }
