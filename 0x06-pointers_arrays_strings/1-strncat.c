4be802a9129916c07260ced395d5e9bb598a27eb
JorgeCal123/boom.git <git@github.com>
2021-10-14 11:28:45 -0700

    punto 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,30 @@
 #include "main.h"
 /**
- * _strncat - 
+ * _strncat - concatenates two strings with n size (bytes)
+ * @dest: first pointer char
+ * @src: second pointer char
+ * @int: size in bytes
+ * Return pointer
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
 
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		if (n == j)
+			break;
+		else
+		{
+			dest[i] = src[j];
+			j++;
+			i++;
+		}
+	}
+	dest[i] = '\0';
+	return (dest);
+}
