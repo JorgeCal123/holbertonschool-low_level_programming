5ece70311b294913b185b4e14072bc3d2fb38b43
JorgeCal123/boom.git <git@github.com>
2021-10-15 15:48:37 -0700

    punto 3df

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,20 +5,21 @@
  * Return: pointer char
  */
 char *cap_string(char *s)
-	{
-	char cadena[] ={',', ';', '.', '!', '?', '(', ')', '"', '{', '}', ' ', '\n', '\t'};
+{
+	char cadena[] = {',', ';', '.', '!', '?', '(',
+		')', '"', '{', '}', ' ', '\n', '\t'};
 	int i;
 	int j;
-	
-	for (i = 0 ;s[i] != '\0'; i++)
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0;cadena[j] != '\0'; j++)
+		for (j = 0; c adena[j] != '\0'; j++)
 		{
-			if(s[i] == cadena[j])
+			if (s[i] == cadena[j])
 			{
-				if(s[i + 1] >= 'a' && s[i+1] <= 'z')
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 					s[i + 1] = s[i + 1] - 32;
-			}	
+			}
 		}
 	}
 return (s);
