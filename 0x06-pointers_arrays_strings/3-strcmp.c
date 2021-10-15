a9bd6abe99f1dd241bc1fb54457de0425c1e7c1f
JorgeCal123/boom.git <git@github.com>
2021-10-14 20:26:45 -0700

    punto 3 corr 4

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,11 +9,15 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 	int answer = 0;
+	int stop = 1;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0' && stop)
 	{
 		if (s1[i] != s2[i])
+		{
 			answer = *s1 - *s2;
+			stop = 0;
+		}
 		i++;
 	}
 	return (answer);
