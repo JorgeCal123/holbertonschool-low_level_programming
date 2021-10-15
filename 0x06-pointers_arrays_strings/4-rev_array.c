c816155b95aef0b0343177131335c9f80071a165
JorgeCal123/boom.git <git@github.com>
2021-10-14 21:23:37 -0700

    punto 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_array - function that reverses the content of an array of integers.
+ * @q: first integer
+ * @n: second integer
+ */
+void reverse_array(int *a, int n)
+{
+	int k;
+	char tmp;
+
+	for (k = n - 1; k >= n / 2; k--)
+	{
+		tmp = a[k];
+		a[k] = a[n - k - 1];
+		a[n - k - 1] = tmp;
+	}
+}
