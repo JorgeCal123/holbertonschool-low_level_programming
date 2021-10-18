f1ed8b976db375f7de387f09cb35ee7e4ca38bce
JorgeCal123/boom.git <git@github.com>
2021-10-18 16:48:32 -0700

    punto 3 corr 6

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,18 +7,25 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0, j, k;
+	int j, k;
 	unsigned int res = 0;
+	int continuar1 = 1;
+	int continuar2 = 1;
 
-	while (accept[i] != '\0')
-		i++;
-
-	for (j = 0; s[j] != '\0'; j++)
+	for (j = 0; s[j] != '\0' && continuar1; j++)
 	{
-		for (k = 0; k <= i; k++)
+		continuar2 = 1;
+
+		for (k = 0; accept[k] != '\0' && continuar2; k++)
 		{
-			if (s[j]) == accept [k])
+			if (s[j] == accept[k])
+			{
 				res++;
+				continuar1 = 1;
+				continuar2 = 0;
+			}
+			else
+				continuar1 = 0;
 		}
 	}
 	return (res);
