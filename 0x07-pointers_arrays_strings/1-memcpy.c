eb9bd42d68a9cc669c67cec1f37f5ddc8392b592
JorgeCal123/boom.git <git@github.com>
2021-10-18 10:00:44 -0700

    punto 0

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _memcpy - function that copies memory area.
+ * @dest: pointer char to modify
+ * @src: pointer char to add
+ * @n; size unsigned int
+ * Return: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+}
