72c2e04cfa08e23ad2f016da0599928a36c03253
JorgeCal123/boom.git <git@github.com>
2021-10-22 12:23:49 -0700

    punto 0 corr 1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,8 @@
  * @argv: An array containing the program command line arguments
  * Return: value 0
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (argc);
 }
