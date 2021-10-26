dcf0cd509ef707e550a439468f9c276260564607
JorgeCal123/boom.git <git@github.com>
2021-10-26 16:57:25 -0700

    p

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,7 +6,7 @@
  * _strdup - function that returns a pointer to a newly allocated space
  * in memory, which contains a copy of the string given as a parameter.
  * @str: pointer type char
- * Return: pointer to the duplicated string or NULL if insufficient 
+ * Return: pointer to the duplicated string or NULL if insufficient
  * memory was available
  */
 char *_strdup(char *str)
@@ -16,7 +16,9 @@ char *_strdup(char *str)
 	int size = strlen(str) + 1;
 
 	p = malloc(sizeof(char) * size);
-	if (size != 0 && str != NULL)
+	if (str == NULL)
+		return (NULL);
+	else if (size != 0 && str != NULL)
 	{
 		for (i = 0; i < size; i++)
 			p[i] = str[i];
