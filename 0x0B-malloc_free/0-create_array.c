6b50fb12f10277ac97857b5dbaf271d02d54ced5
JorgeCal123/boom.git <git@github.com>
2021-10-26 13:41:04 -0700

    punto 0 corr 1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	p = malloc(sizeof(char) * size);
-	if (size != 0 || p != NULL)
+	if (size != 0 && p != NULL)
 	{
 		for (i = 0; i < size; i++)
 			p[i] = c;
