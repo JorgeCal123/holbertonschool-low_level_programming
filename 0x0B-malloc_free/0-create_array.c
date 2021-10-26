883b3b418564f8d3c4f99d70f45421861f29de0a
JorgeCal123/boom.git <git@github.com>
2021-10-26 13:29:36 -0700

    punto 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - function that creates an array of chars, and initializes it
+ * with a specific char.
+ * @size: lenght of arrays
+ * @c: value type char
+ * Return: pointer char
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i;
+
+	p = malloc(sizeof(char) * size);
+	if (size != 0 || p != NULL)
+	{
+		for (i = 0; i < size; i++)
+			p[i] = c;
+		return (p);
+	}
+	else
+		return (NULL);
+}
