b75bc94e5c5e967118e9f8f2f10fc6d64603374c
JorgeCal123/boom.git <git@github.com>
2021-10-26 20:24:11 -0700

    punto 2 corr 3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,26 +13,30 @@ char *str_concat(char *s1, char *s2)
 {
 	char *p;
 	int i, j;
-	int size_s1;
-	int size_s2;
+	int size_s1 = 0;
+	int size_s2 = 0;
 
-	if(s1 != NULL && s2 != NULL)
+	if (s1 != NULL)
+		size_s1 = strlen(s1);
+	if (s2 != NULL)
+		size_s2 = strlen(s2);
+	if (s1 != NULL && s2 != NULL)
 	{
-		size_s1 = strlen(s1) ;
-		size_s2 = strlen(s2) ;
-		p = malloc(sizeof(char) * (size_s1 + size_s2));
+		size_s1 = strlen(s1);
+		size_s2 = strlen(s2);
+		p = malloc(sizeof(char) * (size_s1 + size_s2) + 1);
 	for (i = 0; i < size_s1; i++)
 	{
 		p[i] = s1[i];
 	}
 	i = 0;
 	for (j = size_s1; j < (size_s1 + size_s2); j++)
-	{       
+	{
 		p[j] = s2[i];
 		i++;
 	}
 	return (p);
 	}
 	else
-		return (NULL);	
+		return (NULL);
 }
