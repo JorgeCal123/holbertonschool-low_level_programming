7897bd3a8cf984b5a648de86e95d143ed6f0a52f
JorgeCal123/boom.git <git@github.com>
2021-10-28 11:00:46 -0700

    punto 0 corr 1

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,15 +1,16 @@
 #include "main.h"
 #include <stdlib.h>
-/*
- * malloc_checked -  function that allocates memory using malloc
+/**
+ * malloc_checked - function that allocates memory using malloc
  * @b: value unsigned int
+ * Return: pointer type void
  */
 void *malloc_checked(unsigned int b)
 {
 	void *p;
-	
+
 	p = malloc(b);
 	if (p == NULL)
-		exit (98);
-	return(p);
+		exit(98);
+	return (p);
 }
