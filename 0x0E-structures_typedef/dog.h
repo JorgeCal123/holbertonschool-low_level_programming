903417307d7a662fc4aafe7bf796d53f125af445
JorgeCal123/boom.git <git@github.com>
2021-11-01 15:50:14 -0700

    punto 1 corr 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,16 +5,15 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: name of owner
- *
- * Description: type dog 
+ * Description: type dog
  */
-typedef struct dog dog_t;
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
