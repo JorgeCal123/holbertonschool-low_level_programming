edb9bf80944685a068a09d16aae5220e8742cef0
JorgeCal123/boom.git <git@github.com>
2021-11-01 20:01:36 -0700

    punto 4 corr 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * new_dog - function that creates a new dog.
  * @name: name of dog
@@ -10,10 +11,22 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *new_chandoso;
+	char *cp_name, *cp_owner;
 
 	new_chandoso = malloc(sizeof(struct dog));
 	if (new_chandoso == NULL)
 		return (NULL);
+	cp_name = malloc(sizeof(char) * (strlen(name) + 1));
+	if (cp_name == NULL)
+		return (NULL);
+	cp_owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (cp_owner == NULL)
+		return (NULL);
+
+	while (*name)
+		*cp_name++ = *name++;
+	while (*owner)
+		*cp_owner++ = *owner++;
 
 	new_chandoso->name = name;
 	new_chandoso->age = age;
