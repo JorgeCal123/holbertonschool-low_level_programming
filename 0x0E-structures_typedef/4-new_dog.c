9125b5d85f5ae3d6838e076badd9263f178dc720
JorgeCal123/boom.git <git@github.com>
2021-11-01 20:31:03 -0700

    punto 4 corr 8

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,11 +14,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *cp_name, *cp_owner;
 	int i = 0, j = 0;
 
-	if (name == NULL || age <= 0 || owner == NULL)
-		return (NULL);
 	new_chandoso = malloc(sizeof(struct dog));
 	if (new_chandoso == NULL)
 		return (NULL);
+	if (name == NULL || age <= 0 || owner == NULL)
+	{
+		free (new_chandoso);
+                return (NULL);
+	}
 	cp_name = malloc(sizeof(char) * (strlen(name) + 1));
 	if (cp_name == NULL)
 		return (NULL);
