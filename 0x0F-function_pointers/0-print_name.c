13ec031e2a55c72655c82e8bd717e7c93a6a189d
JorgeCal123/boom.git <git@github.com>
2021-11-03 13:40:19 -0700

    punto 0 corr 2

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 /**
  * print_name - function that prints a name.
  * @name: char pointer type char
@@ -6,5 +7,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name != NULL || f != NULL)
 	(*f)(name);
 }
