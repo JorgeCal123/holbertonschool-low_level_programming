e03d9847998a8465bf973cd47676d3d7a6238fc7
JorgeCal123/boom.git <git@github.com>
2021-11-03 14:01:29 -0700

    punto1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,13 +1,14 @@
 #include "function_pointers.h"
+#include <stddef.h>
 /**
- * array_iterator - function that executes a function given as a 
+ * array_iterator - function that executes a function given as a
  * parameter on each element of an array.
  * @array: pointer type int
  * @size: is the size of the array
  * @action: is a pointer to the function you need to use
  * Return: void
  */
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
