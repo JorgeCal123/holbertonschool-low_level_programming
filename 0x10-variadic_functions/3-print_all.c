4f9f120b8d5ecb15afed930189dd219e158708b9
JorgeCal123/boom.git <git@github.com>
2021-11-04 15:53:22 -0700

    ṕunto 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,7 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
 /**
  * print_all - function that prints anything.
  * @format: is a list of types of arguments passed to the function
@@ -7,5 +9,52 @@
 void print_all(const char * const format, ...)
 {
 	va_list list_all;
-	int
+	int i = 0;
+	char arg_c;
+	int arg_i;
+	float arg_f;
+	char* arg_s;
+	int size = strlen(format);
+	int imp;
+
+	va_start(list_all, format);
+	while (format[i])
+	{
+		imp = 1;
+		switch(format[i])
+		{
+		case 'c':
+		{
+			arg_c = va_arg(list_all, int);
+			printf("%c", arg_c);
+			break;
+		}
+		case 'i':
+		{
+			arg_i = va_arg(list_all, int);
+			printf("%d", arg_i);
+			break;
+		}
+		case 'f':
+		{
+			arg_f = va_arg(list_all, double);
+			printf("%f", arg_f);
+			break;
+		}
+		case 's':
+		{
+			arg_s = va_arg(list_all, char *);
+			printf("%s", arg_s);
+			break;
+		}
+		default:
+			imp = 0;
+			break;
+		}
+		if (i < size - 1 && imp == 1)
+			printf(", ");
+	i++;
+	}
+	printf("\n");
+	va_end(list_all);
 }
