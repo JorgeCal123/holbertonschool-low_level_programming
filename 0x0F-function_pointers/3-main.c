8d95af730920e151dc3f30621570fe066b0d3ecd
JorgeCal123/boom.git <git@github.com>
2021-11-03 20:49:06 -0700

    punto 3 corr 4

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,7 +17,11 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	oprt = get_op_func(argv[2]);
 	if (!oprt)
 	{
