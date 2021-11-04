50e2aab42961897d6233628c9fe9d8e20b77d208
JorgeCal123/boom.git <git@github.com>
2021-11-04 13:14:04 -0700

    punto 1 corr 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,19 +4,19 @@
 /**
  * print_numbers - function that prints numbers, followed by a new line
  * @separator: constant type char
- * @n:  is the number of integers passed to the function
+ * @n: is the number of integers passed to the function
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list list_number;
 	unsigned int i = 0;
 	int arg;
-	va_start(list_number, n); 
 
+	va_start(list_number, n);
 	while (i < n)
 	{
 		arg = va_arg(list_number, int);
-		if (i != n-1)
+		if (i != n - 1)
 			printf("%d%s", arg, separator);
 		else
 			printf("%d\n", arg);
