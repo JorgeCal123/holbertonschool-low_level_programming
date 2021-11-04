c27378d1392f855da745066716a09944b7723da3
JorgeCal123/boom.git <git@github.com>
2021-11-03 20:36:12 -0700

    punto 4

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * main - function main
+  * @argc: size of
+  * @argv: pinter type char
+  * Return: value of operation
+  */
+int main(int argc, char *argv[])
+{
+	int (*oprt)(int, int);
+	int resultado;
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	oprt = get_op_func(argv[2]);
+	if (!oprt)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	resultado = oprt(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", resultado);
+	return (0);
+}
