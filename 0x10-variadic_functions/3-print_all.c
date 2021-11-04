5b484645772c1c21d63c3223635409f2d302acaa
JorgeCal123/boom.git <git@github.com>
2021-11-04 16:01:21 -0700

    punto 3 corr 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,44 +9,33 @@
 void print_all(const char * const format, ...)
 {
 	va_list list_all;
-	int i = 0;
+	int i = 0, arg_i, size = strlen(format), imp;
 	char arg_c;
-	int arg_i;
 	float arg_f;
-	char* arg_s;
-	int size = strlen(format);
-	int imp;
+	char *arg_s;
 
 	va_start(list_all, format);
 	while (format[i])
 	{
 		imp = 1;
-		switch(format[i])
+		switch (format[i])
 		{
 		case 'c':
-		{
 			arg_c = va_arg(list_all, int);
 			printf("%c", arg_c);
 			break;
-		}
 		case 'i':
-		{
 			arg_i = va_arg(list_all, int);
 			printf("%d", arg_i);
 			break;
-		}
 		case 'f':
-		{
 			arg_f = va_arg(list_all, double);
 			printf("%f", arg_f);
 			break;
-		}
 		case 's':
-		{
 			arg_s = va_arg(list_all, char *);
 			printf("%s", arg_s);
 			break;
-		}
 		default:
 			imp = 0;
 			break;
