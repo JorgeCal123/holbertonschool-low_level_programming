52f6223d09a5d20bcd8e08f23b1e4bebdf4495d2
JorgeCal123/boom.git <git@github.com>
2021-11-04 20:44:02 -0700

    punto 3 corr 9

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -15,7 +15,7 @@ void print_all(const char * const format, ...)
 	char *arg_s;
 
 	va_start(list_all, format);
-	while (format[i])
+	while (format[i] && format != NULL)
 	{
 		imp = 1;
 		switch (format[i])
