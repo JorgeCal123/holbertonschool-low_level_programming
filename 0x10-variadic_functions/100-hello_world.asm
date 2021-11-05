f59775193c248b145d1a0de0a227f9301ca0ba9e
JorgeCal123/boom.git <git@github.com>
2021-11-04 19:25:08 -0700

    punto 3 corr 6

--- 0x10-variadic_functions/100-hello_world.asm
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -1,15 +1,15 @@
 section	.text
 	global main
 main:
-	mov	edx, len
-	mov	ecx, msg
-	mov	ebx, 1
-	mov	eax, 4
-	int	0x80
-	mov	eax, 1
-	int	0x80
+	mov edx, len
+	mov ecx, msg
+	mov ebx, 1
+	mov eax, 4
+	int 0x80
+	mov eax, 1
+	int 0x80
 
 section	.data
 
-msg	db	'Hello, world!',0xa
-len	equ	$ - msg	
+msg db 'Hello, world!',0xa
+len equ	$ - msg
