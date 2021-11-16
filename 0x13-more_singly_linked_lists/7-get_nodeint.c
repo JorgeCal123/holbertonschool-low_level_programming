42127deac420c0ff13eb3245e404b483248ea523
JorgeCal123/boom.git <git@github.com>
2021-11-16 13:01:40 -0800

    punto 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - function that returns the nth node
+ * of a listint_t linked list.
+ * @head: pointer type listint_t
+ * @index: is the index of the node, starting at 0
+ * Return: node type listint_t
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int n = 0;
+	listint_t *node;
+
+	while (head != NULL && n != index)
+	{
+		head = head->next;
+		node = head;
+		n++;
+	}
+	return (node);
+}
