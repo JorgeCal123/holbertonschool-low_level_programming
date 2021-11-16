8bc736bd2603ed505e500a36e6de29e543f7d792
JorgeCal123/boom.git <git@github.com>
2021-11-16 09:20:54 -0800

    punto 2 corr 1

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,7 +9,7 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *tmp;
-	
+
 	tmp = malloc(sizeof(listint_t));
 	if (tmp == NULL)
 	{
@@ -20,6 +20,6 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	tmp->next = *head;
 	*head = tmp;
 
-	return (*head);
+	return (head);
 
 }
