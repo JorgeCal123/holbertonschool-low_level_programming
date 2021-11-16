0393a2c463828462d42ade393f3649bec2824bd7
JorgeCal123/boom.git <git@github.com>
2021-11-16 12:20:14 -0800

    punto 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * pop_listint - function that deletes the head node of a listint_t
+ * @head: doble pointer type listint_t
+ * Return: The head node’s data (n)
+ */
+int pop_listint(listint_t **head)
+{
+	int node;
+	listint_t *cp_head;
+
+	cp_head = (*head)->next;
+	node =  (*head)->n;
+	free(*head);
+	*head = cp_head;
+	return (node);
+}
