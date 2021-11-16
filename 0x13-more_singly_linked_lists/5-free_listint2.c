9d25277051cb720d9a76d4956ee8feeb95724e1d
JorgeCal123/boom.git <git@github.com>
2021-11-16 11:18:06 -0800

    punto 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,15 +3,14 @@
  * free_listint2 - function that frees a listint_t list
  * @head: pointer type listint_t that have all nodes
  */
-void free_listint2(listint_t *head)
+void free_listint2(listint_t **head)
 {
-	if (head == NULL)
-	{
-		*head = NULL;
+	if (*head == NULL)
 		return;
-	}
-	free_listint(head->next);
-	free(head);
+	free_listint2(&(*head)->next);
+	free(*head);
+	*head = NULL;
+
 }
 
 
