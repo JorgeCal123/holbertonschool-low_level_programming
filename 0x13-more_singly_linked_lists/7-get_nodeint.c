a639e0cb98314904c74e39fb74f05266da0e5ae2
JorgeCal123/boom.git <git@github.com>
2021-11-16 13:31:12 -0800

    punto 7 corr 6

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,13 +11,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int n = 0;
 	listint_t *node;
 
-	while (head != NULL && n != index)
+	while (head != NULL && n < index)
 	{
 		head = head->next;
 		node = head;
 		n++;
 	}
-	if (head == NULL || node == NULL)
+	if (head == NULL)
                 return (NULL);
 
 	return (node);
