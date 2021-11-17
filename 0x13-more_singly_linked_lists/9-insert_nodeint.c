5c94959b2d17b2fc73a47a8fc12d49606de16fde
JorgeCal123/boom.git <git@github.com>
2021-11-16 17:54:45 -0800

    punto 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - function that inserts a new node
+ * at a given position
+ * @head: doble pointer type listint_t
+ * @idx: is the index of the list where the new node should
+ * be added. Index starts at 0
+ * @n: is value integer of node
+ * Return: the address of the new node, or NULL if it failed
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *nuevo;
+	listint_t *actual;
+	listint_t *anterior;
+	listint_t *siguiente;
+	unsigned int posicion = 0;
+
+	nuevo = malloc(sizeof(listint_t));
+	if (nuevo == NULL)
+	{
+		free(nuevo);
+		return NULL;
+	}
+	actual = *head;
+	while (actual != NULL)
+	{
+		if (posicion == (idx - 1))
+			anterior = actual;
+
+		if (posicion == (idx))
+		{
+			siguiente = actual;
+			break;
+		}
+		actual = actual->next;
+		posicion++;
+	}
+	nuevo->n = n;
+	anterior->next = nuevo;
+	nuevo->next = siguiente;
+	return (nuevo);
+}
