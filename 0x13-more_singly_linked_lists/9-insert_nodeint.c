348ad462a45cba88fd9122495c7a0038e6bc2284
JorgeCal123/boom.git <git@github.com>
2021-11-16 18:02:16 -0800

    punto 9 corr 2

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *siguiente;
 	unsigned int posicion = 0;
 
-	if (head == NULL);
+	if (head == NULL)
 		return (NULL);
 
 	nuevo = malloc(sizeof(listint_t));
