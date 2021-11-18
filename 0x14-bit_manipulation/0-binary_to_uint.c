eaa2de8b37beb33705a8f41e2dcc72971b81b713
JorgeCal123/boom.git <git@github.com>
2021-11-18 10:53:30 -0800

    punto 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * _pow - function that do operations with potency
+ * @n: base number
+ * @p: exponent number
+ * Return: potency
+ */
+unsigned int _pow(unsigned int n, unsigned int p)
+{
+	int resultado = n;
+
+	if (p == 0)
+		return (1);
+	while (p > 1)
+	{
+		resultado = resultado * n;
+		p--;
+	}
+	return (resultado);
+}
+
+/**
+ * binary_to_uint - function that converts a binary number to an
+ * unsigned int.
+ * @b: value how binary
+ * Return: value in decimal
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal = 0;
+	int i = strlen(b) - 1;
+	int a = 2;
+	int exp = 0;
+
+	while (i >= 0)
+	{
+		if (b[i] >= '0' && b[i] <= '9')
+		{
+		if (b[i] == '1')
+			decimal += _pow(a, exp);
+		}
+		else
+			return (0);
+	i--;
+	exp++;
+	}
+	return (decimal);
+}
