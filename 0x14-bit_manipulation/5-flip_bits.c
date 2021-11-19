2dec5ae1c5abb1204e599f43e2d9b2299785fd0c
JorgeCal123/boom.git <git@github.com>
2021-11-18 19:50:18 -0800

    punto 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -6,3 +6,16 @@
  * @m: number
  * Return: number of bits
  */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int i = 0;
+
+	while (n != 0 || m != 0)
+	{
+		if ((n & 1) != (m & 1))
+			i++;
+		m = m >> 1;
+		n = n >> 1;
+	}
+	return (i);
+}
