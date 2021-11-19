bc36cb524750a0e42c6582729b4f03469225c375
JorgeCal123/boom.git <git@github.com>
2021-11-18 20:05:38 -0800

    punto 0 corr 8

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -30,7 +30,7 @@ unsigned int binary_to_uint(const char *b)
 
 	while (i >= 0)
 	{
-		if (b[i] < '0' && b[i] > '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		if (b[i] == '1')
 			decimal += ope;
