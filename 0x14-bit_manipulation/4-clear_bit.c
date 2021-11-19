10dc2cd8675c453078ac262f4302bb4af2ed46fd
JorgeCal123/boom.git <git@github.com>
2021-11-18 18:34:47 -0800

    punto 4 corr 2

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,6 +11,8 @@ int clear_bit(unsigned long int *n, unsigned int index)
 
 	if (n == NULL)
 		return (-1);
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
 
 	bits = 1 << index;
 	if ((bits | *n) == *n)
