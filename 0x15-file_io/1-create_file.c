cc8e63dcb2e01e075b80c39f03ba1273dde65c89
JorgeCal123/boom.git <git@github.com>
2021-11-23 13:57:44 -0800

    punto 1 corr 4

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,15 +18,15 @@ int create_file(const char *filename, char *text_content)
 	if (file_open == -1)
 		return (-1);
 
-	if (text_content == NULL)
-		return (-1);
+	if (text_content != NULL)
+	{
 	while (text_content)
 		size++;
 
 	file_write = write(file_open, text_content, size);
 	if (file_write == -1)
 		return (-1);
-
+	}
 	close(file_open);
 	return (1);
 }
