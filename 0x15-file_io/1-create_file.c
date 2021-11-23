4d399c7ab9b461e07eba11517fdb2b8945eb53df
JorgeCal123/boom.git <git@github.com>
2021-11-23 13:52:16 -0800

    punto 1 corr 3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file_open = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	file_open = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file_open == -1)
 		return (-1);
 
