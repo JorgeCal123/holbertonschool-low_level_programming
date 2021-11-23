c913f7b1173deb059c0480ede0db2d205230d283
JorgeCal123/boom.git <git@github.com>
2021-11-23 10:51:13 -0800

    punto 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * read_textfile - function that reads a text file and prints
+ * it to the POSIX standard output.
+ * @filename: text file
+ * @letters: is the number of letters it should read and print
+ * Return: the actual number of letters it could read and print
+ * if the file can not be opened or read, return 0
+ * if filename is NULL return 0
+ * if write fails or does not write the expected amount of bytes,
+ * return 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file_open;
+	ssize_t file_read;
+	char *save =  malloc(sizeof(char *) * letters);
+
+	if (filename == NULL)
+		return (0);
+	
+	file_open = open(filename, O_RDONLY);
+	if (file_open == -1)
+		return (0);
+
+	file_read = read(file_open, save, letters);
+	if (file_read == 0)
+                return (0);
+	
+	write(STDOUT_FILENO, save, file_read);
+	close(file_open);
+	return (file_read);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+/*libraries standar*/
+#include <stddef.h>
+#include <stdlib.h>
+/*libraries file io*/
+#include <fcntl.h>
+#include <unistd.h>
+/*prototypes Mandatory*/
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+#endif
