8e24305624471c16df382c303b72bff46d15740d
JorgeCal123/boom.git <git@github.com>
2021-11-23 18:43:00 -0800

    punto 3 corr 4

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 	ssize_t numRead;
 	char buf[BUF_SIZE];
 
-	if (argc != 3 )
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	openFlags = O_CREAT | O_WRONLY | O_TRUNC;
-	filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP |S_IROTH; 
+	filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	/* rw-rw-r- */
 	outputFd = open(argv[2], openFlags, filePerms);
 	if (outputFd == -1)
@@ -49,14 +49,14 @@ int main(int argc, char *argv[])
                 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
                         if (close(inputFd) == -1)
 			{
-				dprintf(2, "Error: Can't close fd %d\n", inputFd);
+				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", inputFd);
 				exit(100);
 			}
 
 			if (close(outputFd) == -1)
 			{
-			dprintf(2, "Error: Can't close fd %d\n", outputFd);
-			exit(100);
+				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", outputFd);
+				exit(100);
 			}
        		}
 
@@ -69,13 +69,13 @@ int main(int argc, char *argv[])
 	}
 		if (close(inputFd) == -1)
                         {
-                                dprintf(2, "Error: Can't close fd %d\n", inputFd);
+                                dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", inputFd);
                                 exit(100);
                         }
 
                         if (close(outputFd) == -1)
                         {
-                        dprintf(2, "Error: Can't close fd %d\n", outputFd);
+                        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", outputFd);
                         exit(100);
                         }
 
