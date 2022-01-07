b4981752031e50020da14b185f4f012097ba7a52
JorgeCal123/boom.git <git@github.com>
2022-01-06 17:26:03 -0800

    punto 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add_dnodeint - function that adds a new node at the
+ * beginning of a dlistint_t list.
+ * @head: type dlistint_t list.
+ * @n: number type int
+ * Return: the address of the new element, or NULL if it failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *nuevo;
+	dlistint_t *tmp;
+	tmp = *head;
+
+	nuevo = malloc(sizeof(dlistint_t));
+	if (nuevo == NULL)
+		return (NULL);
+	
+	nuevo->n = n;
+	if (tmp == NULL)
+	{
+		nuevo->prev = NULL;
+		nuevo->next = NULL;
+		*head = nuevo;
+	}
+	else
+	{
+		nuevo->prev = tmp->prev;
+		nuevo->next = tmp;
+		*head= nuevo;
+	}
+	return (*head);
+}
