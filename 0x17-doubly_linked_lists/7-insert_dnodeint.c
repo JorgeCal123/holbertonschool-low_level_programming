ba412e3a8fac02b469ab351c6bea43e6341c2eb5
JorgeCal123/boom.git <git@github.com>
2022-01-06 20:20:17 -0800

    punto 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * insert_dnodeint_at_index - unction that inserts a new node at a given position.
+ * insert_dnodeint_at_index - function that inserts a new node at a
+ * given position.
  * @h: list type dlistint_t
- * @idx: index of the list where the new node should be added. Index starts at 0
+ * @idx: index of the list where the new node should be added.
+ * Index starts at 0
  * @n: number type int
- * Returns: the address of the new node, or NULL if it failed
+ * Return: the address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
@@ -15,14 +17,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-
 	temp = *h;
 	while ((counter < (idx - 1)) && temp)
 	{
 		temp = temp->next;
 		counter++;
 	}
-
 	if (counter < idx - 1)
 		return (NULL);
 
@@ -32,7 +32,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->n = n;
 	new_node->prev = temp;
 	new_node->next = temp->next;
