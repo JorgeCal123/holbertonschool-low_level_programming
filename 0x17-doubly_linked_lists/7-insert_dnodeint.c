a71229ac4b0223cba7f6e6e48495453e3755bca7
JorgeCal123/boom.git <git@github.com>
2022-01-06 20:11:28 -0800

    punto 7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,42 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - unction that inserts a new node at a given position.
+ * @h: list type dlistint_t
+ * @idx: index of the list where the new node should be added. Index starts at 0
+ * @n: number type int
+ * Returns: the address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int counter = 0;
+	dlistint_t *temp, *new_node;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	temp = *h;
+	while ((counter < (idx - 1)) && temp)
+	{
+		temp = temp->next;
+		counter++;
+	}
+
+	if (counter < idx - 1)
+		return (NULL);
+
+	if ((counter == idx - 1) && (temp->next == NULL))
+		return (add_dnodeint_end(h, n));
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = temp;
+	new_node->next = temp->next;
+	temp->next->prev = new_node;
+	temp->next = new_node;
+	return (new_node);
+}
