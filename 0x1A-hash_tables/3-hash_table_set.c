443fd156239724091b834e02682b07840804d73b
JorgeCal123/boom.git <git@github.com>
2022-01-28 16:17:49 -0800

    punto 3 corr 1

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL)
 		return (0);
-	if (key == NULL || *key == "\0")
+	if (key == NULL || *key == '\0')
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	tmp = NULL;
