afcb76636b78b1f260da5bc0eb9bb119270f0db0
JorgeCal123/boom.git <git@github.com>
2022-03-21 13:40:59 -0700

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""Island Perimeter"""
+
 
 def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid"""
+
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
