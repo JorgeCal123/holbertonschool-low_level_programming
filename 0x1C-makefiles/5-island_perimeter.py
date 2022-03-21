1669c66e1a2a0cf5c5050466b610be1666e1d549
JorgeCal123/boom.git <git@github.com>
2022-03-21 13:58:34 -0700

    task 5 corr 3

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,13 +9,12 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if (grid[i][j] == 1):
-                perimeter += 4 
-                if (grid[i - 1][j] == 1):
-                    perimeter -= 1
-                if (grid[i + 1][j] == 1):
-                    perimeter -= 1
-                if (grid[i][j + 1] == 1):
-                    perimeter -= 1
-                if (grid[i][j - 1] == 1):
-                    perimeter -= 1
+                if (i - 1) == -1 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if (i + 1) == nrows or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if (j - 1) == -1 or grid[a][j - 1] == 0:
+                    perimeter += 1
+                if (j + 1) == ncolumns or grid[i][j + 1] == 0:
+                    perimeter += 1
     return (perimeter)
